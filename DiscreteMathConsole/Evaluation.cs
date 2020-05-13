75bb13a1231920ffbe951d4f892bf83003cfb7fe
Arthur Sultanov <as@boolqa.com>
2020-05-13 17:12:43 +0300

    base version

---
+++ DiscreteMathConsole/Evaluation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiscreteMathConsole
+{
+    public static class Evaluation
+    {
+        public static (string pdnf, string pcnf) GetPdnfPcnf(string functionResults)
+        {
+            var functionVector = functionResults.Select(f => f == '1').ToArray();
+
+            var variablesCount = (int) Math.Log(functionVector.Length, 2);
+
+            if (functionResults.Length != 1 << variablesCount)
+                throw new InvalidOperationException();
+
+            var pcnfValues = new List<string>();
+            var pdnfValues = new List<string>();
+
+            for (var i = 0; i < functionVector.Length; i++)
+                if (functionVector[i])
+                {
+                    var expressionVariables = new List<string>();
+
+                    for (int index = variablesCount - 1, charCode = 'a'; index >= 0; index--, charCode++)
+                        expressionVariables.Add(
+                            ((i >> index) & 1) == 1
+                                ? $"{(char) charCode}"
+                                : $"!{(char) charCode}"
+                        );
+
+                    pdnfValues.Add(string.Join("", expressionVariables));
+                }
+                else
+                {
+                    var expressionVariables = new List<string>();
+
+                    for (int index = variablesCount - 1, charCode = 'a'; index >= 0; index--, charCode++)
+                        expressionVariables.Add(
+                            ((i >> index) & 1) == 0
+                                ? $"{(char) charCode}"
+                                : $"!{(char) charCode}"
+                        );
+
+                    pcnfValues.Add($"({string.Join(" | ", expressionVariables)})");
+                }
+
+            return (string.Join(" | ", pdnfValues), string.Join(" & ", pcnfValues));
+        }
+    }
+}
---
+++ DiscreteMathConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscreteMathConsole
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var (n1Pdnf, n1Pcnf) = Evaluation.GetPdnfPcnf("00010101");
+            var (n2Pdnf, n2Pcnf) = Evaluation.GetPdnfPcnf("11101011");
+            var (n3Pdnf, n3Pcnf) = Evaluation.GetPdnfPcnf(Console.ReadLine());
+            var result = new StringBuilder()
+                .AppendLine("n1 (16 + 5) 00010101")
+                .AppendLine($"pdnf: {n1Pdnf}")
+                .AppendLine($"pcnf: {n1Pcnf}")
+                .AppendLine("n2 (256 - (16 + 5) 11101011")
+                .AppendLine($"pdnf: {n2Pdnf}")
+                .AppendLine($"pcnf: {n2Pcnf}")
+                .AppendLine("n3 custom expression")
+                .AppendLine($"pdnf: {n3Pdnf}")
+                .AppendLine($"pcnf: {n3Pcnf}")
+                .ToString();
+            Console.WriteLine(result);
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory ?? throw new InvalidOperationException();
+
+            await File.WriteAllTextAsync(Path.Combine(baseDirectory, "result.txt"), result, Encoding.UTF8);
+        }
+    }
+}
